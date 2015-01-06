2077b70cf998c7b71ca38c048f2b0af8bcfc8b13
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2015-01-06 16:13:23 +0800

    Add optional parameters

--- multi_params.rb
+++ multi_params.rb
@@ -4,3 +4,13 @@ end
 
 puts multi_params
 puts multi_params("a", "b", 1)
+
+
+def opt_params(a, *b, c)
+  puts "a = #{a}, b = #{b}, c = #{c}"
+end
+
+#opt_params("a")
+opt_params("a", "c")
+opt_params("a", "b", "c")
+opt_params("a", "b1", "b2", "c")
