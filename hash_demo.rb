7812d03a6e4b809c48ff60017b1bcc8d7ea1e0ed
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2015-01-08 16:17:44 +0800

    Add hash with symbol

--- hash_demo.rb
+++ hash_demo.rb
@@ -3,3 +3,16 @@ puts "length = #{hash.length}"
 puts "hash[\"b\"] = #{hash["b"]}"
 puts "hash = #{hash}"
 puts "hash[\"e\"] = #{hash["e"]}"
+
+symbol_hash = { :a => "A", :b => "B", :c => "C", :d => "D" }
+puts "symbol_hash = #{symbol_hash}"
+
+# another hash
+m_symbol_hash = { a: "A", b: "B", c: "C", d: "D"}
+puts "m_symbol_hash = #{m_symbol_hash}"
+
+# numeric hash is not work
+# n_symbol_hash = { 1: "A" }
+
+n_hash = { 1 => "A" }
+puts n_hash
