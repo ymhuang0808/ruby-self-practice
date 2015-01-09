cc9116391703c033426d2c1f83d79a2cca4df6ae
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2015-01-09 10:31:51 +0800

    加入 File 操作

---
+++ file_manipulation.rb
@@ -0,0 +1,8 @@
+# File.open() 開啟檔案，會丟出 File 物件
+# 如果 code block 執行完後，檔案會自動被關閉
+File.open("default_param.rb", "r") do |f|
+  # 取得每一行，檔案讀取到結束後，會取得 nil，則跳出 while loop
+  while line = f.gets
+    puts line
+  end
+end
