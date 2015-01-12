b9a61b9e2ca6c423ac4d898e468726cb31f7501a
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2015-01-12 15:34:16 +0800

    加入 Open class，Dog 類別增加 #hello

--- dogs.rb
+++ dogs.rb
@@ -4,3 +4,14 @@ require_relative "dog"
 
 dog1 = Dog.new("XiBai", "white")
 puts "#{dog1.my_name} : #{dog1.woof} >>> #{dog1.my_color}"
+
+
+class Dog
+  def hello
+    "Hello, my name is #{@name} and my color is #{@color}"
+  end
+end
+
+puts dog1.hello
+puts self.class
+puts self
