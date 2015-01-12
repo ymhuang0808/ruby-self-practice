f47de5419a107d4f3e73f02dcefd8a1b38765cc3
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2015-01-12 16:20:26 +0800

    繼承覆寫父類別的方法

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -13,3 +13,23 @@ end
 cat = Cat.new
 cat.breathe
 cat.speak
+
+# 類別繼承，方法覆寫
+class Bird
+  def preen
+    puts "我在清羽毛"
+  end
+
+  def 飛
+    puts "我在飛..."
+  end
+end
+
+class Penguin < Bird
+  def 飛
+    puts "我不會飛..."
+  end
+end
+
+企鵝 = Penguin.new
+企鵝.飛
